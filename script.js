2256c66018fb24b0553381eef13b6241fab6f74a
Hana <alharithihana@gmail.com>
2026-05-28 14:16:33 +0300

    First upload

---
+++ script.js
@@ -0,0 +1,508 @@
+
+const image=document.getElementById("pic");
+const source=document.getElementById("source");
+const details=document.getElementById("details");
+const newQuote=document.getElementById("newQ");
+
+
+
+
+
+const tripletsArray
+ = [
+    {
+        image: "images/1.png",
+        source: "Snapchat",
+        details: " SC Story"
+    },
+    {
+        image: "images/2.png",
+        source: "Are we overrated? -Sturniolo Triplets",
+        details: "20:05"
+    },
+    {
+        image: "images/3.png",
+        source: "Snap Chat story",
+        details: "replying to a fan"
+    },
+    {
+        image: "images/4.png",
+        source: "Matt's old tiktok - found on YT 4 (minutes of the sturniolo triplets giving advice)",
+        details: "4:03"
+    },
+    {
+        image: "images/5.png",
+        source: "Matt's old tiktok - found on YT 4 (minutes of the sturniolo triplets giving advice)",
+        details: "4:31"
+    },
+    {
+        image: "images/6.png",
+        source: "What the f**k is up youtube",
+        details: "3:51"
+    },
+    {
+        image: "images/7.png",
+        source: "Christopher Sturniolo Q&A",
+        details: "03:06"
+    },
+    {
+        image: "images/8.png",
+        source: "Christopher Sturniolo Q&A",
+        details: "02:45"
+    },
+    {
+        image: "images/9.png",
+        source: "who is mostly likely (this is shocking)",
+        details: "17:37"
+    },
+    {
+        image: "images/10.png",
+        source: "who is mostly likely (this is shocking)",
+        details: "17:41"
+    },
+    {
+        image: "images/11.png",
+        source: "trying easter peeps but we hate them",
+        details: "13:49"
+    },
+//########################################################################################
+
+    {
+        image: "images/12.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "13:12"
+    },
+
+        {
+        image: "images/13.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "13:33"
+    },
+         {
+        image: "images/14.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "13:39"
+    },
+
+         {
+        image: "images/15.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "13:55"
+    },
+
+
+          {
+        image: "images/16.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "14:42"
+    },
+
+            {
+        image: "images/17.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "16:31"
+    },
+
+               {
+        image: "images/18.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "16:41"
+    },
+
+
+               {
+        image: "images/19.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "17:31"
+    },
+
+                 {
+        image: "images/20.png",
+        source: "Video title - Golfing with dad +Yung Pleit BTS + Mindfulness thoughts",
+        details: "17:40"
+    },
+
+
+
+
+                   {
+        image: "images/21.png",
+        source: "Video title - My last 5 months -Matt's channel",
+        details: "3:48"
+    },
+
+
+    
+                   {
+        image: "images/22.png",
+        source: "Video title - My last 5 months -Matt's channel",
+        details: "8:14"
+    },
+
+
+                     {
+        image: "images/23.png",
+        source: "Video title - My last 5 months -Matt's channel",
+        details: "16:25"
+    },
+
+                            {
+        image: "images/24.png",
+        source: "Video title - My last 5 months -Matt's channel",
+        details: "16:05"
+    },
+
+
+
+                        {
+        image: "images/25.png",
+        source: "Video title - Life update (Q&A pt 3.) More content soon!!!",
+        details: "4:37"
+    },
+
+                           {
+        image: "images/26.png",
+        source: "Video title - Vlog #6 - Sturniolo Triplets",
+        details: "5:02"
+    },
+
+
+                             {
+        image: "images/27.png",
+        source: "Video title - Vlog #7 - Sturniolo Triplets",
+        details: "6:11"
+    },
+
+                                 {
+        image: "images/28.png",
+        source: "Video title - Vlog #7 - Sturniolo Triplets",
+        details: "6:24"
+    },
+
+
+    
+                                 {
+        image: "images/29.png",
+        source: "Video title - Life Update Pt 2 WE'RE IN LA!!",
+        details: "6:31"
+    },
+
+
+                                     {
+        image: "images/30.png",
+        source: "Video title - Dabbing on em in LA !! ",
+        details: "2:16"
+    },
+
+                                         {
+        image: "images/31.png",
+        source: "Video title - Would you rather + Getting Serious ",
+        details: "15:10"
+    },
+
+
+                                             {
+        image: "images/32.png",
+        source: "Video title - Would you rather + Getting Serious ",
+        details: "20:07"
+    },
+
+
+    
+                                             {
+        image: "images/33.png",
+        source: "Video title - were sorry... here's what's going on. ",
+        details: "8:50"
+    },
+
+
+                                                 {
+        image: "images/34.png",
+        source: "Video title - interviewing ourselves a year apart",
+        details: "3:13"
+    },
+
+                                                     {
+        image: "images/35.png",
+        source: "Video title - interviewing ourselves a year apart",
+        details: "3:46"
+    },
+
+
+                                                         {
+        image: "images/36.png",
+        source: "Video title - interviewing ourselves a year apart",
+        details: "6:42"
+    },
+
+                                                             {
+        image: "images/37.png",
+        source: "Video title - interviewing ourselves a year apart",
+        details: "17:04"
+    },
+
+                                                               {
+        image: "images/38.png",
+        source: "Video title - interviewing ourselves a year apart",
+        details: "17:09"
+    },
+
+                                                                   {
+        image: "images/39.png",
+        source: "WE ARGUE ABOUT OUR FAVORITE THINGS IN THE WORLD!",
+        details: "22:22"
+    },
+
+
+                                                                     {
+        image: "images/40.png",
+        source: "500K *MATT LOOSES IT*",
+        details: "3:30"
+    },
+
+
+                                                                        {
+        image: "images/41.png",
+        source: "get to know us! + recap on our growth.",
+        details: "19:37"
+    },
+
+
+                                                                            {
+        image: "images/42.png",
+        source: "We deserve to use the bathroom!",
+        details: "8:23"
+    },
+
+{
+          image: "images/43.png",
+        source: "reacting to your biggest fears!!",
+        details: "12:44"
+    },
+
+
+    {
+          image: "images/44.png",
+        source: "MAKING GIFTS FOR FANS",
+        details: "14:16"
+    },
+
+
+        {
+          image: "images/45.png",
+        source: "MAKING GIFTS FOR FANS",
+        details: "13:41"
+    },
+
+
+        {
+          image: "images/46.png",
+        source: "TRIPLETS BUY EACH OTHER CHRISTMAS GIFTS!!!",
+        details: "27:17"
+    },
+
+
+    
+        {
+          image: "images/47.png",
+        source: "Always bring us the dessert menu..",
+        details: "20:54"
+    },
+
+
+
+          {
+          image: "images/48.png",
+        source: "asking each other unreasonable questions part 2",
+        details: "26:32"
+    },
+
+
+    
+          {
+          image: "images/49.png",
+        source: "WE ARGUE ABOUT OUR FAVORITE THINGS IN THE WORLD! PART 3",
+        details: "22:37"
+    },
+
+
+    
+          {
+          image: "images/50.png",
+        source: "WE LET OUR DOG RULE OUR DAY!",
+        details: "10:43"
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+];
+
+
+function displayRandomQuote(){
+
+const randomIndex=Math.floor(Math.random()*tripletsArray.length);
+
+const current=tripletsArray[randomIndex];
+
+image.src=current.image;
+source.innerText=current.source;
+details.innerText=current.details;
+
+
+
+
+
+
+}
+
+
+newQuote.addEventListener('click',displayRandomQuote);
+
+
+displayRandomQuote();
+
+
+
+
+// this part is from codePen !
+
+
+(function fairyDustCursor() {
+  
+  var possibleColors = ["#fda917", "#ae00ff", "#044ec4"];
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var particles = [];
+  
+  function init() {
+    bindEvents();
+    loop();
+  }
+  
+  function bindEvents() {
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('touchmove', onTouchMove);
+    document.addEventListener('touchstart', onTouchMove);
+    window.addEventListener('resize', onWindowResize);
+  }
+  
+  function onWindowResize(e) {
+    width = window.innerWidth;
+    height = window.innerHeight;
+  }
+  
+  function onTouchMove(e) {
+    if(e.touches.length > 0) {
+      for(var i = 0; i < e.touches.length; i++) {
+        addParticle(e.touches[i].clientX, e.touches[i].clientY, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+      }
+    }
+  }
+  
+  function onMouseMove(e) {    
+    addParticle(e.clientX, e.clientY, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+  }
+
+  function addParticle(x, y, color) {
+    var particle = new Particle();
+    particle.init(x, y, color);
+    particles.push(particle);
+  }
+  
+  function updateParticles() {
+    for(var i = 0; i < particles.length; i++) {
+      particles[i].update();
+    }
+    for(var i = particles.length - 1; i >= 0; i--) {
+      if(particles[i].lifeSpan < 0) {
+        particles[i].die();
+        particles.splice(i, 1);
+      }
+    }
+  }
+  
+  function loop() {
+    requestAnimationFrame(loop);
+    updateParticles();
+  }
+  
+  function Particle() {
+    this.character = "☆";
+    this.lifeSpan = 120;
+    this.initialStyles = {
+      "position": "absolute",
+      "display": "block",
+      "pointerEvents": "none",
+      "z-index": "10000000",
+      "fontSize": "11px",
+      "fontWeight": "bold",
+      "lineHeight": "1",
+      "textAlign": "center"
+    };
+
+    this.init = function(x, y, color) {
+      this.velocity = {
+        x: (Math.random() - 0.5) * 0.8,
+        y: (Math.random() - 0.5) * 0.8 - 0.5
+      };
+      
+      this.position = {x: x, y: y};
+      this.initialStyles.color = color;
+      
+      this.element = document.createElement('span');
+      this.element.innerHTML = this.character;
+      applyProperties(this.element, this.initialStyles);
+      this.update();
+      
+      document.body.appendChild(this.element);
+    };
+    
+    this.update = function() {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      this.lifeSpan--;
+      
+      var scaleValue = 0.3 + (this.lifeSpan / 120) * 0.7;
+      this.element.style.left = this.position.x + "px";
+      this.element.style.top = this.position.y + "px";
+      this.element.style.transform = "translate(-50%, -50%) scale(" + scaleValue + ")";
+    }
+    
+    this.die = function() {
+      if(this.element && this.element.parentNode) {
+        this.element.parentNode.removeChild(this.element);
+      }
+    }
+  }
+  
+  function applyProperties(target, properties) {
+    for(var key in properties) {
+      target.style[key] = properties[key];
+    }
+  }
+  
+  init();
+})();
+
+
+
+
